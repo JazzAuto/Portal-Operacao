8ab8f07c3260013898993db3be2b465cb4f8dba8
JazzAuto <t073548461@cassi.com.br>
2024-09-30 14:10:39 -0300

    Add files via upload
    atualizações da pasta static

---
+++ portal-server/.next/static/development/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static\u002Fchunks\u002Fpages\u002Findex.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/server01":["static\u002Fchunks\u002Fpages\u002Fserver01.js"],sortedPages:["\u002F","\u002F_app","\u002F_error","\u002Fserver01"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
