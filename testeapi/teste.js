0548d96c47bd4b3df98f2db1f58b79a616121d1d
OptimusAuto <t724192961@cassi.com.br>
2024-10-03 11:21:33 -0300

    Update teste.js
    axios

--- testeapi/teste.js
+++ testeapi/teste.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
 async function teste(request, response){
-
+  axios.get(`https://172.16.70.12/api/table.json?content=sensors&output=json&columns=objid,probe,group,device,sensor,status,message,lastvalue,priority,favorite&apitoken=O3DXJ4XITTZPTTNHCQWIT3G3DKT2A7576GDWPVAEL4======`);
+  .then(resultado=>{console.log(resultado.data)})
 }
