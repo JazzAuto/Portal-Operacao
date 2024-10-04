bee9c64e62ec97c2d794db2551dbfd0bee95ddc9
JazzAuto <t073548461@cassi.com.br>
2024-10-04 16:47:54 -0300

    Update teste1.js
    ajuste na função da api e criação do filtro de dados, resolver TODOs pendentes como testes de fetch usando ambiente recem montado

--- testeapi/api/teste1.js
+++ testeapi/api/teste1.js
@@ -2,10 +2,6 @@ import axios from 'axios';
 async function teste1(request, response){
 
   /*
-  TODO: criação do bloco try catch para tratamento de erros de arquivo, rede e funcionamento registrando em:
-  console.log() || arquivo log || retorno simples de erro
-  para melhor análise;
-
   Teste de fetch com novo front/back;
 
   Uso de return imediatamente abaixo de fetch();
@@ -16,9 +12,9 @@ async function teste1(request, response){
   
   //coleta da data atual de tivação da api
   let dynamicDate = new Date();
-  
+  try{
   //função para requisição do json ad PRTG
-  let alertResponse = axios.get(`https://172.16.70.12/api/table.json?content=sensors&output=json&columns=objid,probe,group,device,sensor,status,message,lastvalue,priority,favorite&apitoken=O3DXJ4XITTZPTTNHCQWIT3G3DKT2A7576GDWPVAEL4======`);
+  let alertaResponse = axios.get("https://172.16.70.12/api/table.json?content=sensors&output=json&columns=objid,probe,group,device,sensor,status,message,lastvalue,priority,favorite&apitoken=O3DXJ4XITTZPTTNHCQWIT3G3DKT2A7576GDWPVAEL4======")
   .then(resultado=>{console.log(resultado.data)})
 
   //caso a requisição volte com algum estado diferente de 200(ok)
@@ -27,14 +23,31 @@ async function teste1(request, response){
     }
 
   //conversão da resposta de retorno em arquivo Json
-  const alertResponseJson = /*await????*/ alertResponse.json();
+  const alertResponseJson = await alertaResponse.json();
 
+  
   //formatação do Json de resposta
-  response.json{
+  //verificar lógica para filtragem de dados
+
+ let response={
 
     date: dynamicDate.toGMTString(),
-    //verificar lógica para filtragem de dados 
-  }
-
+    dados: dadosFiltro
+     
 }
-default export teste1;
+
+}catch(error){
+    return("Erro ao tentar acessar a página, tente novamente mais tarde");
+}
+
+  function dadosFiltro(dados){
+    let camposPermitidos=["objid"];
+    return camposPermitidos.reduce((filtro, campo)=>{
+        if(dados[campo]){
+            filtro[campo]=dados[campo];
+        }
+        return filtro;
+    },{});
+  }
+}
+export default  teste1;
